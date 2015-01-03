aab36a4d11416133789fd22fc8a1e089eec18a1f
Tyler Gaw <me@tylergaw.com>
2015-01-03 18:06:26 -0500

    Use rollback to discard unsaved edits to friend models

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  deactivate: function () {
+    var model = this.modelFor('friends/edit');
+
+    if (model.get('isDirty')) {
+      model.rollback();
+    }
+  },
   actions: {
     cancel: function () {
       this.transitionTo('articles', this.modelFor('friends/edit'));
