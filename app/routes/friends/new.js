f8782f83fc53414a748a85b856f7df7bcc72b3f8
Tyler Gaw <me@tylergaw.com>
2015-01-03 18:04:52 -0500

    Uses the deactivate hook to clear unsaved new friend model

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   model: function () {
     return this.store.createRecord('friend');
   },
+  deactivate: function () {
+    var model = this.modelFor('friends/new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
   actions: {
     cancel: function () {
       this.transitionTo('friends');
