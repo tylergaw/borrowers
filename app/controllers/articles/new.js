28b146bb809602645c53f1a263d11e14ba0f5931
Tyler Gaw <me@tylergaw.com>
2015-01-03 15:28:41 -0500

    Cleaning up some routes

--- app/controllers/articles/new.js
+++ app/controllers/articles/new.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.description',
-    function () {
-      return !Ember.isEmpty(this.get('model.description'));
-    }
+  hasDescription: Ember.computed.notEmpty('model.description'),
+  isValid: Ember.computed.and(
+    'hasDescription'
   ),
   actions: {
     save: function () {
--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,7 +3,7 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel: function () {
-      this.transitionToRoute('friends.show', this.get('model'));
+      this.transitionToRoute('articles', this.get('model'));
       return false;
     }
   }
