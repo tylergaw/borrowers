8101c482c3b20693d9266ca3cdcebabe22ff789e
Tyler Gaw <me@tylergaw.com>
2015-01-03 00:22:19 -0500

    Adding all the stuff so far

---
+++ app/models/friend.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  email: DS.attr('string'),
+  twitter: DS.attr('string'),
+  totalArticles: DS.attr('number'),
+  fullName: Ember.computed('firstName', 'lastName', function () {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  })
+});
