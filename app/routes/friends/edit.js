39ea3af23ed79738cc0e7f7e9c1439ed3b3567f4
Tyler Gaw <me@tylergaw.com>
2015-01-03 18:19:35 -0500

    Remove uneeded check for isDirty, rollback handles it

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -3,10 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   deactivate: function () {
     var model = this.modelFor('friends/edit');
-
-    if (model.get('isDirty')) {
-      model.rollback();
-    }
+    model.rollback();
   },
   actions: {
     cancel: function () {
