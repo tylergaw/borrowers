b38e87d27d0977d6c7e30e525f8a13d5cc0bdf7e
Tyler Gaw <me@tylergaw.com>
2015-01-03 20:23:48 -0500

    Introducing moment and date formatter helpers

--- Brocfile.js
+++ Brocfile.js
@@ -16,6 +16,7 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/moment/moment.js');
 app.import('bower_components/picnic/releases/v2.min.css');
 app.import('vendor/fontello/fontello.css');
 app.import('vendor/fontello/font/fontello.ttf', {
