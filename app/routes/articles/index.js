f81de29a7d69712bb1d2a1ef626c29ae6e52229f
Tyler Gaw <me@tylergaw.com>
2015-01-04 22:40:50 -0500

    Keeping commented code for force reloading articles in place

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -2,7 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.modelFor('friends/show').get('articles');
+    var articles = this.modelFor('friends/show').get('articles');
+
+    // NOTE: This doesn't seem necessary in this scenario, but keeping here
+    // for posterity.
+    //
+    // if (articles.get('isFulfilled')) {
+    //   articles.reload();
+    // }
+
+    return articles;
   },
   actions: {
     save: function (model) {
